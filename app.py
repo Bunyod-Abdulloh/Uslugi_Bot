a3c33d9d1b0ebd6092b70c16d41fb6063c8dc28d
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2024-03-04 15:08:36 +0500

    Updated

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 import asyncio
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
 
 from data.config import BOT_TOKEN
-from handlers.users.start import user_private_router
+from handlers.users.start import user_start_router
 from handlers.users.user_main import user_main_router
+from utils.set_bot_commands import private
 
-ALLOWED_UPDATES = ['message, edited_message']
+ALLOWED_UPDATES = ['message, callback_query, inline_query']
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-dp.include_router(user_private_router)
+dp.include_router(user_start_router)
 dp.include_router(user_main_router)
 
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
+    await bot.set_my_commands(
+        commands=private,
+        scope=types.BotCommandScopeAllPrivateChats()
+    )
+    # await on_startup_notify()
     await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
 
--- handlers/users/user_main.py
+++ handlers/users/user_main.py
@@ -1,6 +1,8 @@
 from aiogram import Router, types, F
+from filters.chat_type_filter import ChatTypeFilter
 
 user_main_router = Router()
+user_main_router.message.filter(ChatTypeFilter(['private']))
 
 
 @user_main_router.message(F.text == "📱 Mutaxassis bilan bog'lanish")
@@ -8,3 +10,8 @@ async def communicate_to_specialist(message: types.Message):
     await message.answer(
         text="Mutaxassis turini tanlang"  # mutaxassislar ro'yxati dbdan olinib keyboard shaklida qaytariladi
     )
+
+
+@user_main_router.message(F.photo)
+async def get_photo(message: types.Message):
+    print(message.photo[-1].file_id)
