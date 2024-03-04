34fe7c4752eb27057005a02a1aae85befd63ce59
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2024-03-04 19:08:59 +0500

    Updated

--- handlers/users/user_main.py
+++ handlers/users/user_main.py
@@ -1,5 +1,6 @@
 from aiogram import Router, types, F
 from filters.chat_type_filter import ChatTypeFilter
+from keyboards.inline import user_select_doctors_ibutton
 
 user_main_router = Router()
 user_main_router.message.filter(ChatTypeFilter(['private']))
@@ -8,7 +9,8 @@ user_main_router.message.filter(ChatTypeFilter(['private']))
 @user_main_router.message(F.text == "📱 Mutaxassis bilan bog'lanish")
 async def communicate_to_specialist(message: types.Message):
     await message.answer(
-        text="Mutaxassis turini tanlang"  # mutaxassislar ro'yxati dbdan olinib keyboard shaklida qaytariladi
+        text="Mutaxassis turini tanlang",
+        reply_markup=user_select_doctors_ibutton().as_markup()
     )
 
 
--- keyboards/inline.py
+++ keyboards/inline.py
@@ -1,11 +1,12 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from utils.clinics import doctors
+
 
 def user_select_doctors_ibutton():
-    keyboard = InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                InlineKeyboardButton(
-                    text=
-            ]
-        ]
-    )
+    builder = InlineKeyboardBuilder()
+
+    for doctor in doctors:
+        builder.button(text=doctor, callback_data=doctor)
+    return builder
