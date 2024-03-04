e466229ac909e35ee9c050bbcc64f126426fdf5c
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2024-03-04 12:49:44 +0500

    Updated

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from aiogram import Bot, Dispatcher
 
 from data.config import BOT_TOKEN
 from handlers.users.start import user_private_router
+from handlers.users.user_main import user_main_router
 
 ALLOWED_UPDATES = ['message, edited_message']
 
@@ -10,6 +11,7 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 dp.include_router(user_private_router)
+dp.include_router(user_main_router)
 
 
 async def main():
---
+++ handlers/users/user_main.py
@@ -0,0 +1,11 @@
+from aiogram import Router, types
+from magic_filter import F
+
+user_main_router = Router()
+
+@user_main_router.message(F.text == "📱 Mutaxassis bilan bog'lanish")
+async def communicate_to_specialist(message: types.Message):
+    await message.answer(
+        text="Mutaxassis turini tanlang"  # mutaxassislar ro'yxati dbdan olinib keyboard shaklida qaytariladi
+    )
+
